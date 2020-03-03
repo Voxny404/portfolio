bae637e69a1f58ff53b33b8885a606a111ded44e
Voxny404 <Atomchen92@gmail.com>
2020-03-03 01:34:06 +0100

    adding skills

--- ScriptContact.js
+++ ScriptContact.js
@@ -8,8 +8,8 @@ function showcontact(){
 	/*//text */
 	var text = document.getElementById("Home2").innerHTML = 'Atomchen92@gmail.com <br> <br> <a href="https://github.com/Voxny404" target="_blank">'
 	+'<img alt="Qries" src="https://github.com/fluidicon.png"width=30" height="30">'
-	+'<a href="https://www.youtube.com/user/TheAtomchen" target="_blank">'
-	+'<img alt="Qries" src="https://s.ytimg.com/yts/img/favicon_32-vflOogEID.png"width=30" height="30">';
+	+'<a href="https://asukawhatever.herokuapp.com/Login" target="_blank">'
+	+'<img alt="Qries" src="https://www.herokucdn.com/favicons/icon.svg"width=30" height="30">';
 	
 	
 	/*//Logo */
--- ScriptSkills.js
+++ ScriptSkills.js
@@ -1,17 +1,25 @@
 function showskills(){
 
 	/*//BodyText_____________________________________________________________________________________________________________*/
-
+	let html ='HTML| pug | CSS | ajax'
+	let unity ='Python3 | C# | Unity'
+	let language = 'German | English'
+	let node = 'Node | express'
+	let Enc = 'Encryption at rest'
+	let soc = 'websockets | socket.io'
+	let git = 'GitHub | Git'
+	let Mon = 'MongoDb | Auth'
+	let heroku = 'Heroku | OOP'
 	/*headline  */
-	var text = document.getElementById("Home1").innerHTML = 'Skills';	
+	const text1 = document.getElementById("Home1").innerHTML = 'Skills';	
 
 	/*text */
-	var text = document.getElementById("Home2").innerHTML = 'HTML | CSS <br> Python3 <br> C# | Unity <br> German | English <br>'
-	+' Javascript <br> GitHub | Git <br> ';
+	const text2 = document.getElementById("Home2").innerHTML = html +'<br>'+unity+'<br>'+ language +' <br>'
+	+' Javascript <br> '+ git +' <br>'+ node +'<br>'+Enc+'<br>'+soc+'<br>'+Mon+'<br>'+ heroku+ '<br>';
 	
 
 	/*//Logo */
-	var text = document.getElementById("Logo1").innerHTML = '<img src="Logo1.png" height="420" width="420">';
+	const text3 = document.getElementById("Logo1").innerHTML = '<img src="Logo1.png" height="420" width="420">';
 	document.getElementsByClassName("Logo")[0].style.right = "30%";		/* logoLocading*/
 
 	/*//navibarSelection_____________________________________________________________________________________________________*/ 
