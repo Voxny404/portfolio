2ea39fba980f03995f454ce6ab29ca353b883219
Voxny404 <Atomchen92@gmail.com>
2023-05-31 00:39:55 +0200

    slight update

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 let app = document.getElementById('app');
-const aboutMeText = "About me My name is Jessica Nicole Pötzl. I'm a person who seeks a challenge. Computers have always fascinated me. In my spare time I like to work with unity and create my own games. My very first computer that I owned, I built at the age of 14. In 2019 I rediscovered unity and created my very first game. Another interesting hobby of mine is creating music with the guitar and piano. I've been playing the guitar since 2008 and started playing the piano in 2020. My native language is German, or as I like to call it Bayrisch.";
-const languageText = 'Javascript, Jscript, C#, HTML, CSS, German, English.';
-const frameworksText = 'Node, Electron, React, AWS, Cordova.';
-const otherText = 'Github, JWT, Socket.IO, MongoDb.';
-const projectText = '<a href="https://voxny404.github.io/GameStuff/">Games<a><br>'+
-                    '<a href="https://apitink.herokuapp.com/home">Tinker<a><br>';
+const aboutMeText = "Hello, I'm Jessica Nicole P. , and I thrive on challenges. The world of computers has always captivated me, and I find joy in exploring its endless possibilities. In my spare time, I immerse myself in the world of game development, working with Unity to bring my imaginative ideas to life. Building my first computer at the tender age of 14 ignited my passion for technology, and ever since, I've been driven to push the boundaries of what is possible. In 2019, I reconnected with my love for game development and created my very first game using Unity. This experience solidified my dedication to honing my skills in this dynamic field. As a developer, I constantly seek to learn and improve, eager to take on new projects and embrace the evolving landscape of the gaming industry. Beyond my technological pursuits, I find solace and inspiration in creating music. Since 2008, I have been strumming the strings of the guitar, allowing my emotions to flow through every chord. More recently, in 2020, I embarked on a new journey by learning to play the piano, expanding my musical repertoire and challenging myself to master new melodies. As a multilingual individual, I proudly consider German, my native language. This linguistic background has nurtured my appreciation for cultural diversity and communication, enabling me to connect with people from various walks of life. In my portfolio, you'll find a collection of my game development projects, showcasing my skills, creativity, and dedication. I'm eager to collaborate, grow, and contribute to the ever-evolving world of technology and entertainment. Let's embark on an exciting journey together, where innovation knows no bounds.";
+const languageText = 'Javascript, Golang, C#, CSS, German, English.';
+const frameworksText = 'ElectronJS, ReactJS, AWS, DynamoDB.';
+const otherText = '<a style="color:#A799B5" href="https://github.com/Voxny404">github.com/Voxny404<a>';
+const projectText = '<a style="color:#A799B5" href="https://voxny404.github.io/GameStuff/">Games<a><br>';
+
 let loadingbar = 0;
 let errorDisplay = false;
 let binaryString = '';
+
 for (var i = 0; i < 100; i++) {
   binaryString = binaryString + Math.floor(Math.random()*2).toString()
 }
@@ -25,7 +26,6 @@ const createCanvas = () => {
   let div = document.createElement('div');
   div.style.height = (window.innerHeight / 3) + 'px';
   let canvas = document.createElement('canvas');
-  let ctx = canvas.getContext('2d');
 
   canvas.width = window.innerWidth;
   canvas.height = (window.innerHeight / 3) + 10;
@@ -59,11 +59,11 @@ const updateCanvas = () => {
     ctx.font = '70px Arial';
     ctx.textAlign = 'center';
     ctx.fillStyle = 'red';
-    ctx.fillText('ERROR', (canvas.width / 2) - 6, (canvas.height / 2) - 2);
+    ctx.fillText('SUCCESS', (canvas.width / 2) - 6, (canvas.height / 2) - 2);
     ctx.fillStyle = 'cyan';
-    ctx.fillText('ERROR', (canvas.width / 2) + 2, (canvas.height / 2) + 2);
+    ctx.fillText('SUCCESS', (canvas.width / 2) + 2, (canvas.height / 2) + 2);
     ctx.fillStyle = 'white';
-    ctx.fillText('ERROR', canvas.width / 2, canvas.height / 2);
+    ctx.fillText('SUCCESS', canvas.width / 2, canvas.height / 2);
     ctx.font = Math.floor(Math.random()*70)+'px Arial';
     ctx.fillText('▀',(Math.floor(Math.random()*canvas.width)), (Math.floor(Math.random()*canvas.height)));
     setTimeout(() => {
@@ -81,7 +81,7 @@ const updateCanvas = () => {
     ctx.fillText('Voxny404   ', (canvas.width / 2) + 3, (canvas.height / 2) + 3);
     ctx.font = '25px Arial';
     ctx.fillStyle = 'gray';
-    ctx.globalAlpha = 0.2;
+    ctx.globalAlpha = 0.5;
     ctx.fillText(binaryString, (Math.floor(Math.random()*canvas.width)), (Math.floor(Math.random()*canvas.height)));
   }
   if (randomizer == 1 && !errorDisplay) {
@@ -153,13 +153,12 @@ const createMainContent = () => {
   let aboutme =  document.createElement('h2');
   let aboutmeP =  document.createElement('p');
   let languages =  document.createElement('h3');
-  let languagesP =  document.createElement('button');
+  let languagesP =  document.createElement('p');
   let frameworks =  document.createElement('h3');
-  let frameworksP =  document.createElement('button');
+  let frameworksP =  document.createElement('p');
   let other =  document.createElement('h3');
   let otherP =  document.createElement('button');
-  let projects = document.createElement('h2');
-  let projectsP = document.createElement('p');
+  let otherP2 =  document.createElement('button');
   let footerP = document.createElement('p');
   let footerA = document.createElement('p');
 
@@ -172,10 +171,10 @@ const createMainContent = () => {
   mainContentDiv.appendChild(frameworksP);
   mainContentDiv.appendChild(other);
   mainContentDiv.appendChild(otherP);
+  mainContentDiv.appendChild(otherP2);
   mainContentDiv.appendChild(document.createElement('br'));
   mainContentDiv.appendChild(document.createElement('br'));
-  mainContentDiv.appendChild(projects);
-  mainContentDiv.appendChild(projectsP);
+
   imageDiv.appendChild(button);
 
   aboutme.innerText = 'About me';
@@ -185,9 +184,8 @@ const createMainContent = () => {
   frameworks.innerText = 'Frameworks';
   frameworksP.innerText = frameworksText;
   other.innerText = 'Other';
-  otherP.innerText = otherText;
-  projects.innerText = 'Projects';
-  projectsP.innerHTML = projectText;
+  otherP.innerHTML = otherText;
+  otherP2.innerHTML = projectText;
   footerA.innerHTML = 'Made by Voxny404@gmail.com';
   footerP.innerHTML = 'powered by Github';
 
@@ -206,35 +204,36 @@ const createMainContent = () => {
   frameworks.style.marginRight = '5%';
   frameworksP.style.marginRight = '8%';
   other.style.marginRight = '5%';
-  otherP.style.marginRight = '5%';
-
+  otherP.style.marginRight = '2%';
   languagesP.style.height = '45px';
   frameworksP.style.height = '45px';
   otherP.style.height = '45px';
+  otherP2.style.height = '45px';
   languagesP.style.paddingLeft = '15px';
   frameworksP.style.paddingLeft = '15px';
   otherP.style.paddingLeft = '15px';
+  otherP2.style.paddingLeft = '15px';
   languagesP.style.paddingRight = '15px';
   frameworksP.style.paddingRight = '15px';
   otherP.style.paddingRight = '15px';
+  otherP2.style.paddingRight = '15px';
 
-  button.innerHTML = '<a href="mailto:Voxny404@gmail.com">CONTACT ME<a>';
+  button.innerHTML = '<a style="color:#A799B5" href="mailto:Voxny404@gmail.com">CONTACT ME<a>';
   button.style.display = 'block';
   button.style.width = '250px';
   button.style.height = '50px';
   button.style.margin = 'auto';
   button.style.marginTop = '10%';
+  button.style.cursor = 'pointer'
 
   if (window.innerWidth >= 540) {
     imageDiv.style.marginRight = '5%';
+    imageDiv.style.paddingTop = '4em'
     imageDiv.style.float = 'right';
-    aboutmeP.style.maxWidth = (window.innerWidth / 2) + 'px';
+    aboutmeP.style.width = (window.innerWidth / 2) + 'px';
+    //aboutmeP.style.maxWidth = '800px'
   }
-
-  projects.style.textAlign = 'center';
-  projectsP.style.textAlign = 'center';
-
-  mainContentDiv.appendChild(document.createElement('br'));
+  
 
   footerA.style.color = '#A799B5';
   footerA.style.fontSize = '15px';
@@ -243,6 +242,8 @@ const createMainContent = () => {
   footer.appendChild(footerA);
   footer.appendChild(footerP);
 
+  mainContentDiv.appendChild(document.createElement('br'));
+  mainContentDiv.appendChild(document.createElement('br'));
   document.body.appendChild(footer);
 
 };
